1c8c739d3c8edf5d854cb642a7d625fb24258dc4
appuser <appuser@journey2-69bc5f84cc-hqpd2>
2022-08-31 14:46:49 +0000

    starter project

---
+++ functions.js
@@ -0,0 +1,22 @@
+function addTwoNumbers(a, b) {
+	//write your code here
+
+}
+
+function isLeftGreaterThanRight(leftNumber, rightNumber) {
+	//write your code here
+	
+}
+
+function sayHello(name) {
+	//write your code here
+
+}
+
+
+//DO NOT MODIFY THE FOLLOWING PART
+module.exports = {
+	addTwoNumbers,
+	isLeftGreaterThanRight,
+	sayHello
+};
